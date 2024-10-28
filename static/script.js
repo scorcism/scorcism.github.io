0898ef4e41fc40fe0ff420a58f9498afe8bae00e
scorcism <abhishekpathak1720@gmail.com>
2024-10-28 23:21:08 +0530

    update ui

--- static/script.js
+++ static/script.js
@@ -1,1 +1,26 @@
-console.log("Henlo World")
+const links = [
+  {
+    url: "https://www.linkedin.com/in/abhishekpathak32",
+    text: "linkedIn/abhishekpathak32",
+  },
+  { url: "https://github.com/scorcism", text: "github/scorcism" },
+  { url: "https://dev.to/scorcism", text: "dev.to/scor32k" },
+  { url: "https://x.com/scor32k", text: "x/scorcism" },
+  { url: "https://buymeacoffee.com/scorcism", text: "buymeacoffee.com/scorcism" },
+];
+
+const linksContainer = document.getElementById("links");
+
+links.forEach((link) => {
+  const linkElement = document.createElement("a");
+  linkElement.href = link.url;
+  linkElement.className = "link";
+  linkElement.target = "_blank";
+
+  const textElement = document.createElement("span");
+  textElement.className = "link-text";
+  textElement.textContent = link.text;
+
+  linkElement.appendChild(textElement);
+  linksContainer.appendChild(linkElement);
+});
